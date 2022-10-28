ad1dc0d84002c23943a95ee7dca8761e07919c3c
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-28 12:24:49 +0200

    ora l'api (BACK-END) può essere chiamata dall'applicazione client (FRONT-END)

--- src/main/java/com/soprasteria/todorestapi/controller/TodoController.java
+++ src/main/java/com/soprasteria/todorestapi/controller/TodoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,8 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.soprasteria.todorestapi.entity.Todo;
 import com.soprasteria.todorestapi.service.TodoService;
 
+@CrossOrigin
 @RestController
-@RequestMapping("/todo")
+@RequestMapping("/api/todo")
 public class TodoController {
 	
 	@Autowired
